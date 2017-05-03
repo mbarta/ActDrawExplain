5f9384d5ca3aa0f1cf991654918a3fc93b341640
Milan Barta <milanbarta@gmail.com>
2017-05-03 20:06:32 +0100

    Fullscreen immersive mode

--- app/src/main/java/me/barta/actdrawexplain/menu/MainMenuActivity.kt
+++ app/src/main/java/me/barta/actdrawexplain/menu/MainMenuActivity.kt
@@ -3,6 +3,7 @@ package me.barta.actdrawexplain.menu
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import me.barta.actdrawexplain.R
 import me.barta.actdrawexplain.databinding.ActivityMainMenuBinding
 
@@ -11,6 +12,18 @@ class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setUpBinding()
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
     }
 
     private fun setUpBinding() {
